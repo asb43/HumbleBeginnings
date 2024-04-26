9b02bff694148fdbdc2a9f769f472a087f27f204
Zachary Craig <Pokemonfanboy600@gmail.com>
2024-04-25 22:58:38 -0700

    fixed filepath of the Menu template manager

--- EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/menu_template_manager.gd
+++ EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/menu_template_manager.gd
@@ -1,6 +1,6 @@
 extends Node
-const OptionConstants = preload("res://addons/EasyMenus/Scripts/options_constants.gd")
-const InputMapUpdater = preload("res://addons/EasyMenus/Scripts/input_map_updater.gd")
+const OptionConstants = preload("C:/GodotProjects/HumbleBeginningsv.2/EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/options_constants.gd")
+const InputMapUpdater = preload("C:/GodotProjects/HumbleBeginningsv.2/EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/input_map_updater.gd")
 
 @onready var ControllerEchoInputGenerator = $ControllerEchoInputGenerator
 @onready var startup_loader = $StartupLoader
