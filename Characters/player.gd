fb421e0074139b494a51e1bb4dd5fe4e2361668d
Zachary Craig <Pokemonfanboy600@gmail.com>
2024-05-03 00:51:45 -0700

    Reworked camera system and how it relates to player movement to make future animations actually work as expected, also input the skeleton3d and animation player/tree for the future animations

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,29 +1,26 @@
 extends CharacterBody3D
-
-
+@onready var camera = $Camera_Controller
+@onready var animation_player = $MeshInstance3D/AnimationPlayer
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var jumps = 0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var pitch_input := 0.0
-var twist_input := 0.0
-var mouse_sensitivity := 0.001
-@onready var twist_pivot := $Camera_Controller
-@onready var pitch_pivot := $Camera_Controller/Camera_Target
+
+@export var sens_horizontal = .5
+@export var sens_vertical = .5
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x*sens_horizontal))
+		camera.rotate_x(deg_to_rad(-event.relative.y* sens_vertical))
+
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	twist_pivot.rotate_y(twist_input)
-	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(
-		pitch_pivot.rotation.x,deg_to_rad(-30),deg_to_rad(30))
-	twist_input = 0.0
-	pitch_input = 0.0
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -41,7 +38,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -50,10 +47,3 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
