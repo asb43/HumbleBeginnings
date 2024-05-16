19d919f5766891a8b50d9e5bade55ff76d094c94
Zachary Craig <47993076+asb43@users.noreply.github.com>
2024-05-15 20:07:50 -0700

    commiting

--- EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/menu_template_manager.gd
+++ EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/menu_template_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 const OptionConstants = preload("res://EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/game_options_constants.gd")
-const InputMapUpdater = preload("C:/GodotProjects/HumbleBeginningsv.2/EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/input_map_updater.gd")
+const InputMapUpdater = preload("C:/Users/pokem/GodotProjects/HumbleBeginnings/EasyMenus-825d5c0607cc4b3f0af636f4903527518404383c/addons/EasyMenus/Scripts/input_map_updater.gd")
 
 @onready var ControllerEchoInputGenerator = $"/root/MenuTemplateManager"/ControllerEchoInputGenerator
 @onready var startup_loader = $"/root/MenuTemplateManager"/StartupLoader
